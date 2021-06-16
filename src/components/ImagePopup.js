2803bd8826e3a1101a8877538377516dc9c4f3cf
kuzinartemiy <kuzin_artemiy@list.ru>
2021-06-16 23:32:30 +0500

    Review 1:fix ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,14 +5,16 @@ function ImagePopup(props) {
   }
 
   const stateClassName = `popup ${props.card ? 'popup_opened' : '' } `;
+  const stateImgSrc = `${props.card ? props.card.link : '#' } `;
+  const stateImgName = `${props.card ? props.card.name : '' } `;
 
   return(
     <div id="photo-popup" className={stateClassName}>
       <div className="popup__photo-container">
-        <button onClick={handleClick} id="photo-card-close-button" aria-label="Закрыть" type="button" className="popup__close-button"></button>
+        <button onClick={handleClick} id="photo-card-close-button" aria-label="Закрыть" type="button" className="popup__close-button"/>
         <figure className="popup__figure">
-          <img className="popup__big-image" src={props.card.link} alt="Фотография большого размера"/>
-          <figcaption className="popup__caption">{props.card.name}</figcaption>
+          <img className="popup__big-image" src={stateImgSrc} alt="Фотография большого размера"/>
+          <figcaption className="popup__caption">{stateImgName}</figcaption>
         </figure>
       </div>
     </div>
